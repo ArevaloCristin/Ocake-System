5b061112a1c7fc48a311510f74b0cd7d4cb19e1d
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2022-12-28 12:34:34 +0800

    Update Routes.php
    Added Route file to ocake repository.

--- ocake/ocake/app/Config/Routes.php
+++ ocake/ocake/app/Config/Routes.php
@@ -36,6 +36,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+
+//---------------- PERSONAL CONTROLLER ROUTING ----------------//
 $routes->get('/personal', 'Personal::index');
 $routes->get('/', 'Personal::signin');
 $routes->get('/signup', 'Personal::signup');
@@ -45,8 +47,9 @@ $routes->get('/signin', 'Personal::signin');
 $routes->post('/save', 'Personal::save');
 $routes->post('/login', 'Personal::login');
 
-// Admin Registration and Login
 
+//------------- ADMINISTRATION CONTROLLER ROUTING -------------//
+// Admin Registration and Login
 $routes->get('/administration', 'Administration::index');
 $routes->get('/', 'Administration::signin');
 $routes->get('/admin-signup', 'Administration::signup');
@@ -57,16 +60,21 @@ $routes->get('/admin-signin', 'Administration::signin');
 $routes->post('/create_account', 'Administration::save');
 $routes->post('/login_account', 'Administration::login');
 
+
+//----------------- ADMIN CONTROLLER ROUTING -----------------//
 $routes->get('/dashboard', 'Admin::dashboard');
 $routes->get('/admin/product', 'Admin::product');
-$routes->get('/admin/orders', 'Admin::orders');
 //$routes->get('/admin/category', 'Admin::category');
+// $routes->get('/login', 'Admin::login');
+// $routes->get('/register', 'Admin::register');
+// $routes->get('/utilities', 'Admin::utilities');
+$routes->get('/forgotpassword', 'Admin::forgotpassword');
+$routes->get('/admin/orders', 'Admin::orders');
+$routes->post('/order/update_order/(:any)', 'Admin::order_update/$1/$2');
 
-//---------- PRODUCT CONTROLLER ROUTING ----------//
+
+//---------------- PRODUCT CONTROLLER ROUTING ----------------//
 $routes->post('/admin/add_product', 'Product::add_product');
-// $routes->get('/', 'Product::get_data');
-// $routes->match(['get', 'post'], 'Product/cat','Product::cat');
-
 
 $routes->get('/admin/category/birthday', 'Product::getBDay');
 $routes->get('/admin/category/christening', 'Product::getChristening');
@@ -77,27 +85,41 @@ $routes->get('/admin/category/halloween', 'Product::getHalloween');
 $routes->get('/admin/category/christmas', 'Product::getChristmas');
 $routes->get('/admin/category/newyear', 'Product::getNewYear');
 $routes->get('/admin/category/new year', 'Product::getNewYear');
-
 $routes->get('/admin/category/delete_prod/(:any)', 'Product::prod_delete/$1/$2');
 $routes->post('/admin/category/update_prod/(:any)', 'Product::prod_update/$1/$2');
-$routes->get('cart/delete_cart/(:any)', 'CartController::CartData_delete/$1/$2');
-$routes->post('/order/update_order/(:any)', 'Admin::order_update/$1/$2');
 
 
-// $routes->get('/login', 'Admin::login');
-// $routes->get('/register', 'Admin::register');
-// $routes->get('/utilities', 'Admin::utilities');
-$routes->get('/forgotpassword', 'Admin::forgotpassword');
+//-------------- CUSTOMIZATION CONTROLLER ROUTING --------------//
+$routes->post('/admin/customization/update_addons/(:any)', 'CustomizationController::addons_update/$1/$2');
+$routes->get('/admin/customization/delete_addons/(:any)', 'CustomizationController::addons_delete/$1/$2');
+$routes->post('/admin/customization/add_addons', 'CustomizationController::add_addons');
+$routes->get('/admin/customization/add_ons', 'CustomizationController::add_ons');
+$routes->post('/admin/customization/add_ons', 'CustomizationController::add_ons');
+
+$routes->post('/admin/customization/update_design/(:any)', 'CustomizationController::design_update/$1/$2');
+$routes->get('/admin/customization/delete_design/(:any)', 'CustomizationController::design_delete/$1/$2');
+$routes->post('/admin/customization/add_design', 'CustomizationController::add_design');
+$routes->get('/admin/customization/design', 'CustomizationController::design');
+$routes->post('/admin/customization/design', 'CustomizationController::design');
 
 
+//------------------ USER CONTROLLER ROUTING ------------------//
 $routes->get('/home', 'User::index');
 $routes->get('/userforgotpassword', 'User::userforgotpassword');
 $routes->get('/about', 'User::about');
 $routes->get('/contact', 'User::contact');
 $routes->get('/customization', 'User::customization');
-$routes->get('/custom', 'User::custom');
+// $routes->get('/custom', 'User::custom');
+// $routes->get('/clone', 'User::clone');
+// $routes->get('/copy', 'User::copy');
+// $routes->get('/paste', 'User::paste');
+// $routes->get('/cust', 'User::cust');
+ $routes->get('/cop', 'User::cop');
+// $routes->get('/add_custom', 'User::add_custom');        //Customization
 $routes->get('/cart', 'User::cart');
 $routes->get('/orderdetails', 'User::orderdetails');
+$routes->post('/cancel_order/(:any)', 'User::cancel_order/$1/$2');
+$routes->post('/order_received/(:any)', 'User::order_received/$1/$2');
 $routes->get('/checkout', 'User::checkout');
 $routes->get('/productlist', 'User::productlist');
 $routes->get('/productgrid', 'User::productgrid');
@@ -115,19 +137,18 @@ $routes->get('/valentine', 'User::getValentine');
 $routes->get('/graduation', 'User::getGrad');
 $routes->get('/newyear', 'User::getNewYear');
 $routes->get('/wedding', 'User::getWedding');
+
+
+//-------------------- CART CONTROLLER ROUTING ---------------------//
 #add cart #
 $routes->post('/add_cart', 'CartController::add_cart');
+$routes->get('cart/delete_cart/(:any)', 'CartController::CartData_delete/$1/$2');
 
+
+//------------------ CHECKOUT CONTROLLER ROUTING ------------------//
 # PLACE ORDER #
 $routes->post('/placeorder','CheckoutController::placeorder');
-// $routes->get('/wedding', 'User::wedding');
-// $routes->get('/graduation', 'User::graduation');
-// $routes->get('/valentine', 'User::valentine');
-// $routes->get('/halloween', 'User::halloween');
-// $routes->get('/christmas', 'User::christmas');
-// $routes->get('/newyear', 'User::newyear');
-// $routes->get('/register', 'User::register');
-// $routes->get('/login', 'User::login');
+
 
 
 
