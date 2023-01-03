b8fb0a0da986c17e81e534f2ab6d53312c1d49cb
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-03 14:18:51 +0800

    Update Checkout_model.php

--- ocake/ocake/app/Models/Checkout_model.php
+++ ocake/ocake/app/Models/Checkout_model.php
@@ -5,33 +5,40 @@ use CodeIgniter\Model;
 
 class Checkout_model extends Model
 {
-    protected $table = 'product_order';
-    // protected $primaryKey ='pid';
-    protected $allowedFields = ['occasion', 'flavor', 'price', 'quantity','stat', 'product_id'];
+    protected $table = 'checkout';
+    // protected $primaryKey ='checkout_id';
+    protected $allowedFields = [ 'user_id', 'biller_id', 'total_price', 'order_code'];
 
-    public function getCartData() {
-        return $this->db->table('tbl_cart as c')
-                        ->select('*')
-                        ->join('product as p','p.id=c.product_id')
+    public function save_checkout($data){
+        // THIS WILL SAVE DATA IN BILLER DETAILS TABLE                              January 02, 2023
+        $insert = $this->insert($data);
+       
+    }
+
+    // public function getCartData($id) {
+    //     return $this->db->table('tbl_cart as c')
+    //                     ->select('*')
+    //                     ->where('c.user_id', $id)
+    //                     ->join('product as p','p.id=c.product_id')
         
-        // ->where('occasion', $occasion)
-                        ->get()->getResult();
-    }
-    function count_data(){
-        return $this->db->table('product_order')
-                    ->selectCount('order_id')
-                    ->get()->getResult();
-    }
-    public function cart_delete($id){
-      $result = $this->db->table('product_order')
-                        ->where('order_id',$id)
-                        ->delete();
+    //     // ->where('occasion', $occasion)
+    //                     ->get()->getResult();
+    // }
+    // function count_data(){
+    //     return $this->db->table('product_order')
+    //                 ->selectCount('order_id')
+    //                 ->get()->getResult();
+    // }
+    // public function cart_delete($id){
+    //   $result = $this->db->table('product_order')
+    //                     ->where('order_id',$id)
+    //                     ->delete();
 
-                        if($result){
-                            return true;
-                        }
+    //                     if($result){
+    //                         return true;
+    //                     }
                     
-      }
+    //   }
 
 }
 ?>
