985574d83058ed7e44527e412c061864a219736f
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-03 14:25:05 +0800

    Update CartController.php
    Updated CartController.php file in ocake repository.

--- ocake/ocake/app/Controllers/CartController.php
+++ ocake/ocake/app/Controllers/CartController.php
@@ -8,50 +8,50 @@ class CartController extends BaseController
     {
         $session = \Config\Services::session();
     }
-    //------------- INSERT PRODUCT DATA ------------//              //November 27, 2022
-
-            
-
-            
-            // echo view('admin/product');
-
-            public function CartData_delete($id){
-                $session = \Config\Services::session();
-                $model = new Cart_m();
-                $model->cart_delete($id);
-
-                // var_dump($a);
+    //------------- INSERT CART DATA ------------//              //November 27, 2022
+    public function add_cart(){     
+        if(isset($_SESSION['logged_in']) == true && isset($_SESSION['type']) == "user"){
+            helper(['form', 'url']);
+            $val = $this->validate([
+                            
+            ]);
         
-                $session->setFlashdata('msg', 'Deleted Successfully!');
-                
-               return redirect('cart');
-            }
+            $model = new Cart_m();
+            $qty ='1';
+            // var_dump($_SESSION['user_id']);
+            $model->insert([
+                'user_id' => $_SESSION['user_id'],
+                'occasion' => $this->request->getVar('occasion'),
+                'flavor' => $this->request->getVar('flavor'),
+                'total_price' => $this->request->getVar('price'), 
+                'price' => $this->request->getVar('price'),
+                'quantity' => $qty,
+                'product_id' => $this->request->getVar('pid'),
+            ]);         
+            return redirect('cart');
+        }else{
+            return $this->response->redirect(site_url('signin'));
+        }                        
+            
+    }
+            
+    //------------- DELETE CART DATA ------------//              //November 27, 2022
+    public function CartData_delete($id){
+        if(isset($_SESSION['logged_in']) == true && isset($_SESSION['type']) == "user"){
+            $session = session();
+            $model = new Cart_m();
+            $model->cart_delete($id);
+
+            $session->setFlashdata('msg', 'Deleted Successfully!');
+            return redirect('cart');
+        }else{
+            return $this->response->redirect(site_url('signin'));
+        } 
+    }
 
 
-            public function add_cart(){                              
-                helper(['form', 'url']);
             
-                /*this will validate inputs */
-                $val = $this->validate([
-                                
-                ]);
             
-                $model = new Cart_m();
-            
-                           $qty ='1';
-                          
-                        $model->insert([
-                            // 'uid' => $this->request->getVar('uid'),
-                            'occasion' => $this->request->getVar('occasion'),
-                            'flavor' => $this->request->getVar('flavor'),
-                            'price' => $this->request->getVar('price'),
-                            'quantity' => $qty,
-                            'product_id' => $this->request->getVar('pid'),
-                            // 'image' => $this->request->getVar('img'),/*this will get the name of file input */
-                        ]);         
-                        return redirect('cart');    
-            }
-            
-        }
+}
     
 ?>
