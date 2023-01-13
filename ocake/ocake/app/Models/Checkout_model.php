d6815bdf2b8239c62edc871e50e56c0d9b0b2151
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-14 07:47:36 +0800

    update checkout model

--- ocake/ocake/app/Models/Checkout_model.php
+++ ocake/ocake/app/Models/Checkout_model.php
@@ -1,109 +1,67 @@
-<?php 
-namespace App\Models;
+<?php
 
-use CodeIgniter\Model;
-
-class Checkout_model extends Model
+namespace App\Controllers;
+use App\Models\Checkout_model;
+use App\Models\Cart_m;
+use App\Models\Biller_model;
+class CheckoutController extends BaseController
 {
-    protected $table = 'checkout';
-    // protected $primaryKey ='checkout_id';
-    protected $allowedFields = [ 'user_id', 'biller_id', 'total_price', 'items', 'order_code', 'stat', 'created_at'];
-
-    //-------------------- INSERT CHECKOUT DATA ----------------------//        January 02, 2023
-    public function save_checkout($data){
-        //this will save data in biller_details table       
-        $insert = $this->insert($data);
-    }
-    //--------------- FETCH CHECKOUT DETAILS FOR ADMIN ---------------//        January 03, 2023
-    # get data in checkout model #
-    public function getCheckoutData($id) {
-        return $this->db->table('checkout as co')
-                        ->select('*')
-                        ->join('users as u','u.id=co.user_id')
-                        ->get()->getResult();
+    public function __construct()
+    {
+        $session = \Config\Services::session();
     }
 
-    //--------------- FETCH CHECKOUT DETAILS FOR ADMIN ---------------//        January 03, 2023
-    # for view order details #
-    public function getDetails($id) {
-        return $this->db->table('checkout as co')
-                        ->select('*')
-                        ->join('biller_details as b','b.biller_id=co.biller_id')
-                        ->join('users as u','u.id=co.user_id')
-                        ->join('cart as c','c.order_code=co.order_code')
-                        ->join('product as p','c.product_id=p.id')
-                        //->where('c.user_id', $id)
-                        ->get()->getResult();
+    //--------------- PLACE ORDER ---------------//           January 02, 2023
+    public function placeorder(){ 
+        if(isset($_SESSION['logged_in']) == true && isset($_SESSION['type']) == "user"){     
+            $id = $_SESSION['user_id'];                 
+            helper(['form', 'url']);
+        
+            $val = $this->validate([ ]);
+            
+            #generate random code
+            $today = date("Ymd");
+            $rand = strtoupper(substr(uniqid(sha1(time())),0,4));
+            $random = $today .$rand;
+
+           #update cart data    
+           $cart_model = new Cart_m();       
+            $update_cart = $cart_model->cart_update($random,$id);
+
+            if($update_cart == true){
+                #insert biller data 
+                $biller_model = new Biller_model();
+                $data = array(       
+                        'firstname'       => $this->request->getVar('firstname'),
+                        'lastname'        => $this->request->getVar('lastname'),
+                        'email'           => $this->request->getVar('email'),
+                        'mobile'          => $this->request->getVar('mobile'),
+                        'municipality'    => $this->request->getVar('municipality'),
+                        'barangay'        => $this->request->getVar('barangay'),
+                        'street'          => $this->request->getVar('street'),
+                        'delivery_method' => $this->request->getVar('delivery_method'),
+                        'date'            => $this->request->getVar('date'),
+                        'payment_method'  => $this->request->getVar('payment_method'),
+                        'user_id'         => $id,       
+                    ); 
+                    $insert_biller = $biller_model->save_biller($data);
+                    
+                     #insert checkout data 
+                    $checkout_model = new Checkout_model();
+                    $datum = array(
+                            'user_id'         => $id, 
+                            'biller_id'       => $insert_biller,
+                            'total_price'     => $this->request->getVar('total_price'),
+                            'items'           => $this->request->getVar('count'),
+                            'order_code'      => $random,
+                    );
+                    $insert_checkout= $checkout_model->save_checkout($datum);
+                    return redirect('cart');  
+            }
+ 
+        } else{
+            return $this->response->redirect(site_url('signin'));
+        }    
     }
-
-
-    //--------------- FETCH ORDER DETAILS DATA FOR USER ---------------//       January 03, 2023
-    # get data in checkout model #
-    public function fetchCheckoutData($id) {
-        return $this->db->table('checkout as co')
-                        ->select('*')
-                        ->join('biller_details as b','b.biller_id=co.biller_id')
-                        ->join('users as u','u.id=co.user_id')
-                        ->join('cart as c','c.order_code=co.order_code')
-                        ->join('product as p','c.product_id=p.id')
-                        ->where('co.user_id', $id)
-                        ->get()->getResult();
-    }
-
-    //--------------- FETCH CHECKOUT DETAILS FOR USER ---------------//        January 03, 2023
-    # get data in checkout model #
-    public function getData($id) {
-        return $this->db->table('checkout as co')
-                        ->select('*')
-                        ->join('biller_details as b','b.biller_id=co.biller_id')
-                        ->join('users as u','u.id=co.user_id')
-                        ->where('co.user_id', $id)
-                        ->groupBy('co.created_at')
-                        ->get()->getResult();
-    }
-
-    //  //--------------- FETCH ORDER DETAILS DATA FOR USER ---------------//       January 03, 2023
-    // # get data in checkout model #
-    // public function fetchCheckoutData($id,$order_code) {
-    //     return $this->db->table('checkout as co')
-    //                     ->select('*')
-    //                     ->join('biller_details as b','b.biller_id=co.biller_id')
-    //                     ->join('users as u','u.id=co.user_id')
-    //                     ->join('cart as c','c.order_code=co.order_code')
-    //                     ->join('product as p','c.product_id=p.id')
-    //                     ->where('c.order_code', $order_code)
-    //                     ->where('co.user_id', $id)
-    //                     ->get()->getResult();
-    // }
-
-    // //--------------- FETCH CHECKOUT DETAILS FOR USER ---------------//        January 03, 2023
-    // # get data in checkout model #
-    // public function getData($id) {
-    //     return $this->db->table('checkout as co')
-    //                     // ->select('*')
-    //                     ->join('biller_details as b','b.biller_id=co.biller_id')
-    //                     ->where('co.user_id', $id)
-    //                     ->groupBy('co.created_at')
-    //                    // ->where('order_date BETWEEN "'. date('y-m-d H:i:s', strtotime($start_date.' 00:00:00')).'" 
-    //                      
-    //                     ->get()->getResult();
-    // }
-
-    //--------------------- UPDATE CHECKOUT DATA ----------------------//       January 03, 2023
-    public function checkout_update($data,$id){
-        $this->set($data)->where('checkout_id', $id)->update();
-    }
-
-    //---------------------- DELETE CHECKOUT DATA ---------------------//       January 03, 2023
-    public function checkout_delete($id){
-        $result = $this->db->table('checkout')
-            ->where('checkout_id', $id)
-            ->delete();
-
-        if ($result) {
-            return true;
-        }
-    }
-
 }
 ?>
