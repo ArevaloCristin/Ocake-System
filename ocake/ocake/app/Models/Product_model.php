9541841a0a81c59ef77259de5e9edc2d5b305e92
ArevaloCristin <120362066+ArevaloCristin@users.noreply.github.com>
2023-01-15 09:37:55 +0800

    Update Product_model.php

--- ocake/ocake/app/Models/Product_model.php
+++ ocake/ocake/app/Models/Product_model.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 class Product_model extends Model{
     protected $table = 'product';
    // protected $primaryKey = 'id';
-    protected $allowedFields = ['occasion', 'status', 'flavor', 'price', 'image'];
+    protected $allowedFields = ['occasion', 'status', 'flavor', 'price', 'image', 'is_customized', 'userid', 'message'];
     
     // public function getBDay($id = false) {
     //   if($id === false) {
@@ -18,7 +18,7 @@ class Product_model extends Model{
 
     # ------ FETCH ALL PRODUCT ----#
     public function fetchProduct() {
-      return $this->select('*')
+      return $this->where('is_customized', 0)
                   ->get()->getResult();
   }
         //---------- CATEGORY RETRIEVE ----------//  November 28,2022
@@ -61,6 +61,10 @@ class Product_model extends Model{
       return $this->select('*')->where('occasion', $occasion)
                   ->get()->getResult();
     }
+    public function getCustomized($occasion) {
+      return $this->select('*')->where('occasion', $occasion)
+                  ->get()->getResult();
+    }
 
     //---------- DELETE PRODUCT ----------//  November 28,2022
 
@@ -72,6 +76,24 @@ class Product_model extends Model{
   
     public function prod_update($data, $id){
       $this->set($data)->where('id', $id)->update();
+    }
+    //---------- INSERT PRODUCT ----------//  November 29,2022
+    public function insertDesign($user_id, $img, $message, $flavor, $price, $status) {
+        $data = [
+            'image' => $img,
+            'occasion' => 'Customized',
+            'message' => $message,
+            'flavor' => $flavor,
+            'price' => $price,
+            'is_customized' => 1,
+            'userid' => $user_id,
+            'status' => $status
+		];
+
+		$result = $this->insert($data);
+    $insert_id = $this->getInsertID();
+		if ($result)
+			return $insert_id;
     }
 
     
