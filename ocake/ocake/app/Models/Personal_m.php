0c7095bad831f6d9479dc152f1d75e1b64e00d8e
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-15 22:07:50 +0800

    Update Personal_m.php
    added new functionality for counting users

--- ocake/ocake/app/Models/Personal_m.php
+++ ocake/ocake/app/Models/Personal_m.php
@@ -4,7 +4,7 @@ use CodeIgniter\Model;
 class Personal_m extends Model{
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['firstname', 'lastname', 'municipality', 'barangay', 'email', 'password'];
+    protected $allowedFields = ['firstname', 'lastname', 'mobile', 'email', 'password'];
 
     //---------- FETCH PERSONAL DATA ----------//  December 27,2022 -->Edited on December 31,2022
     public function fetchPersonal($id) {
@@ -13,45 +13,10 @@ class Personal_m extends Model{
                     ->get()->getResult();
     }
 
-    // public function user_login($uname, $pass){
-	// 	$this->db->where('user_uname', $uname);
-	// 	$query = $this->db->get('user');
-	// 	if($query->num_rows()>0){
-	// 		foreach($query->result() as $row){
-	// 			if($row->user_status ==2){
-	// 				$store_pass = $this->encrypt->decode($row->user_pwd);
-	// 				if($pass == $store_pass){
-	// 					$user_session = array(
-	// 						'id' => $row->user_id,
-	// 						'username' => $row->user_uname,
-	// 						'type' => 'user'
-	// 					);
-	// 					$data= $this->session->set_userdata($user_session);
-	// 					return 'ok';
-	// 				}else{
-	// 					return 'Wrong Password';
-	// 				}
-	// 			}else if($row->user_status ==1){
-	// 				$store_pass = $this->encrypt->decode($row->user_pwd);
-	// 				if($pass == $store_pass){
-	// 					$user_session = array(
-	// 						'id' => $row->user_id,
-	// 						'username' => $row->user_uname,
-	// 						'type' => 'user'
-	// 					);
-	// 					$data= $this->session->set_userdata($user_session);
-	// 					return 'ok';
-	// 				}else{
-	// 					return 'Wrong Password';
-	// 				}
-	// 			}else if($row->user_status ==0){
-	// 				return 'Not Verified';
-	// 			}else{
-	// 				return false;
-	// 			}
-	// 		}
-	// 	}else{
-	// 		return 'Wrong Username';
-	// 	}
-	// }
+    //------------- COUNT USERS --------------//   January 14,2023
+	public function count_users(){
+		return $this->db->table('users')
+					->select('Count(id) as count')
+					->get()->getResult();
+	}
 }
