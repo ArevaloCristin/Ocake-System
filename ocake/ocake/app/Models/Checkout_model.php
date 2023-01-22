db85659763585ae0585ebb897f0d39781fb62b03
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-22 14:26:54 +0800

    update checkout model

--- ocake/ocake/app/Models/Checkout_model.php
+++ ocake/ocake/app/Models/Checkout_model.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 class Checkout_model extends Model
 {
     protected $table = 'checkout';
-    // protected $primaryKey ='checkout_id';
+    // $primaryKey ='checkout_id';
     protected $allowedFields = [ 'user_id', 'biller_id', 'total_price', 'items', 'order_code', 'stat', 'created_at'];
 
     //-------------------- INSERT CHECKOUT DATA ----------------------//        January 02, 2023
@@ -16,23 +16,22 @@ class Checkout_model extends Model
     }
     //--------------- FETCH CHECKOUT DETAILS FOR ADMIN ---------------//        January 03, 2023
     # get data in checkout model #
-    public function getCheckoutData($id) {
+    public function getCheckoutData() {
         return $this->db->table('checkout as co')
                         ->select('*')
+                        ->join('biller_details as b','b.biller_id=co.biller_id')
                         ->join('users as u','u.id=co.user_id')
                         ->get()->getResult();
     }
 
     //--------------- FETCH CHECKOUT DETAILS FOR ADMIN ---------------//        January 03, 2023
     # for view order details #
-    public function getDetails($id) {
+    public function getDetails() {
         return $this->db->table('checkout as co')
                         ->select('*')
-                        ->join('biller_details as b','b.biller_id=co.biller_id')
                         ->join('users as u','u.id=co.user_id')
                         ->join('cart as c','c.order_code=co.order_code')
                         ->join('product as p','c.product_id=p.id')
-                        //->where('c.user_id', $id)
                         ->get()->getResult();
     }
 
@@ -86,33 +85,6 @@ class Checkout_model extends Model
     }
    
 
-     //--------------- FETCH ORDER DETAILS DATA FOR USER ---------------//       January 03, 2023
-    # get data in checkout model #
-    // public function fetchCheckoutData($id,$order_code) {
-    //     return $this->db->table('checkout as co')
-    //                     ->select('*')
-    //                     ->join('biller_details as b','b.biller_id=co.biller_id')
-    //                     ->join('users as u','u.id=co.user_id')
-    //                     ->join('cart as c','c.order_code=co.order_code')
-    //                     ->join('product as p','c.product_id=p.id')
-    //                     ->where('c.order_code', $order_code)
-    //                     ->where('co.user_id', $id)
-    //                     ->get()->getResult();
-    // }
-
-    // //--------------- FETCH CHECKOUT DETAILS FOR USER ---------------//        January 03, 2023
-    // # get data in checkout model #
-    // public function getData($id) {
-    //     return $this->db->table('checkout as co')
-    //                     // ->select('*')
-    //                     ->join('biller_details as b','b.biller_id=co.biller_id')
-    //                     ->where('co.user_id', $id)
-    //                     ->groupBy('co.created_at')
-    //                    // ->where('order_date BETWEEN "'. date('y-m-d H:i:s', strtotime($start_date.' 00:00:00')).'" 
-    //                      
-    //                     ->get()->getResult();
-    // }
-
     //--------------------- UPDATE CHECKOUT DATA ----------------------//       January 03, 2023
     public function checkout_update($data,$id){
         $this->set($data)->where('checkout_id', $id)->update();
@@ -137,6 +109,14 @@ class Checkout_model extends Model
 					->get()->getResult();
 	}
 
+    //---------------------- COUNT CONFIRMED ORDERS ---------------------//       January 14,2023
+	public function count_confirmed(){
+		return $this->db->table('checkout')
+					->select('Count(checkout_id) as count')
+                    ->where('stat',"Confirmed")
+					->get()->getResult();
+	}
+
     //---------------------- COUNT SHIPPED ORDERS ---------------------//       January 14,2023
 	public function count_shipped(){
 		return $this->db->table('checkout')
@@ -144,6 +124,15 @@ class Checkout_model extends Model
                     ->where('stat',"Shipped")
 					->get()->getResult();
 	}
+
+    //---------------------- COUNT PENDING ORDERS ---------------------//       January 14,2023
+	public function count_delivered(){
+		return $this->db->table('checkout')
+					->select('Count(checkout_id) as count')
+                    ->where('stat',"Delivered")
+					->get()->getResult();
+	}
+
     //--------------------- COUNT CANCELLED ORDERS --------------------//       January 14,2023
 	public function count_cancelled(){
 		return $this->db->table('checkout')
@@ -157,6 +146,24 @@ class Checkout_model extends Model
 		return $this->db->table('checkout')
 					->select('Count(checkout_id) as count')
                     ->where('stat',"Completed")
+					->get()->getResult();
+	}
+
+    //------------------------- COUNT EARNINGS ------------------------//      January 14,2023
+	public function count_earnings(){
+		return $this->db->table('checkout')
+					->select('Sum(total_price) as sum')
+                    ->where('stat',"Completed")
+					->get()->getResult();
+	}
+
+    //------------------------- COUNT EARNINGS ------------------------//      January 21,2023
+	public function count_orders($id){
+		return $this->db->table('checkout')
+					->select('Count(checkout_id) as count')
+                    ->where('user_id', $id)
+                    ->where ('stat !=', "Completed")
+                    ->where('stat !=', "Cancelled")
 					->get()->getResult();
 	}
 }
