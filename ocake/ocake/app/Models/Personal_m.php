cc077ba38e771cc6532d99106432a76c021e8821
Jechel Ramirez <115635701+jechelramirez@users.noreply.github.com>
2023-01-29 19:56:13 +0800

    update personal model

--- ocake/ocake/app/Models/Personal_m.php
+++ ocake/ocake/app/Models/Personal_m.php
@@ -4,7 +4,7 @@ use CodeIgniter\Model;
 class Personal_m extends Model{
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['firstname', 'lastname', 'mobile', 'email', 'password'];
+    protected $allowedFields = ['firstname', 'lastname', 'mobile', 'email', 'password', 'verification_code', 'is_verified'];
 
     //---------- FETCH PERSONAL DATA ----------//  December 27,2022 -->Edited on December 31,2022
     public function fetchPersonal($id) {
@@ -19,4 +19,34 @@ class Personal_m extends Model{
 					->select('Count(id) as count')
 					->get()->getResult();
 	}
+
+    public function save_user($data){                        
+        $insert = $this->insert($data);
+       
+        //this will get last id then insert in checkout table as biller's id 
+        // if ($insert){
+        //     return $this->insertID();
+        // }
+        // else{
+        //     return false;
+        // }
+    }
+
+    public function create_accounts($fname, $lname, $email, $mobile, $pass, $code) {
+        $bind = [
+            'firstname' => $fname,
+            'lastname' => $lname,
+            'mobile' => $mobile,
+            'email' => $email,
+            'password' => $this->passwordhash($pass),
+            'verification_code' => $code,
+            'user_verification' => 'no',
+        ];
+
+        if ($bind > 0) {
+            return $this->db->table('users')->insert($bind);
+        } else {
+            echo 'already exist';
+        }
+    }
 }
